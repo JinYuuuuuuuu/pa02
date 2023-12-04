a05968bab514a73be24ec3541cef1fffb0d78400
Hechenjin Yu <hechenjin@cs.ucsb.edu>
2023-12-03 18:12:15 -0800

    11th:

--- movies.cpp
+++ movies.cpp
@@ -14,33 +14,45 @@ void Movies::printAllMovies() const {
     }
 }
 
-pair<string, double> Movies::findAndPrintMoviesWithPrefix(const string& prefix) const {
-    using PairType = pair<double, string>;
-    priority_queue<PairType, vector<PairType>, MovieComparator> pq;
+#include <vector>
+#include <algorithm>
 
-    pair<string, double> highestRatedMovie = {"", -1};
+std::pair<std::string, double> Movies::findAndPrintMoviesWithPrefix(const std::string& prefix) const {
+    std::vector<std::pair<double, std::string>> matchingMovies;
+    std::pair<std::string, double> highestRatedMovie = {"", -1};
 
+    // Collect movies that match the prefix
     for (const auto& movie : movieMap) {
         if (movie.first.find(prefix) == 0) {
-            pq.push(make_pair(movie.second, movie.first));
+            matchingMovies.emplace_back(movie.second, movie.first);
             if (movie.second > highestRatedMovie.second) {
                 highestRatedMovie = movie;
             }
         }
     }
 
-    if (pq.empty()) {
-        cout << "No movies found with prefix " << prefix << endl;
+    // Check if no movies were found
+    if (matchingMovies.empty()) {
+        std::cout << "No movies found with prefix " << prefix << std::endl;
         return highestRatedMovie;
     }
 
-    while (!pq.empty()) {
-        auto movie = pq.top();
-        pq.pop();
-        cout << movie.second << ", " << movie.first << endl;
+    // Sort the vector in descending order of ratings, 
+    // and in case of a tie, sort alphabetically by movie names
+    std::sort(matchingMovies.begin(), matchingMovies.end(), 
+        [](const std::pair<double, std::string>& a, const std::pair<double, std::string>& b) {
+            if (a.first == b.first) {
+                return a.second < b.second; // Sort alphabetically for ties
+            }
+            return a.first > b.first; // Sort by descending ratings
+        });
+
+    // Print the sorted movies
+    for (const auto& movie : matchingMovies) {
+        std::cout << movie.second << ", " << movie.first << std::endl;
     }
 
-    cout << endl;
+    std::cout << std::endl;
 
     return highestRatedMovie;
 }
